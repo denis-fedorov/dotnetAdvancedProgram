06bac7e200951e576fde4c06fef301df908279b9
Denis Fedorov <denis_fedorov@epam.com>
2022-10-24 23:08:55 +0400

    Add Infrastructure and Application projects

---
+++ CatalogService/src/Application/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Interfaces;
+
+public interface IApplicationDbContext
+{
+    public DbSet<Category> Categories { get; }
+
+    public DbSet<Item> Items { get; }
+}
---
+++ CatalogService/src/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Application.Interfaces;
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence;
+
+public class ApplicationDbContext : DbContext, IApplicationDbContext
+{
+    public DbSet<Category> Categories => Set<Category>();
+
+    public DbSet<Item> Items => Set<Item>();
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> dbContextOptions)
+        : base(dbContextOptions)
+    {
+        // do nothing
+    }
+}
