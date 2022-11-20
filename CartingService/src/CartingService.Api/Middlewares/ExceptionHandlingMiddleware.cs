cf40eb102a7660974114a583611380f26bb50b5d
Denis Fedorov <denis_fedorov@epam.com>
2022-11-21 00:39:13 +0400

    Add ExceptionHandlingMiddleware

---
+++ CartingService/src/CartingService.Api/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,43 @@
+﻿namespace CartingService.Api.Middlewares;
+
+public class ExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+    public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+    
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception exception)
+        {
+            await HandleException(context, exception);
+        }
+    }
+
+    private Task HandleException(HttpContext context, Exception exception)
+    {
+        var message = exception.Message;
+        _logger.LogError(exception, "{Message}", message);
+
+        var statusCode = exception switch
+        {
+            // TODO: place all the exceptions here
+            
+            _ => StatusCodes.Status500InternalServerError
+        };
+        
+        context.Response.StatusCode = statusCode;
+        context.Response.ContentType = "text/plain";
+
+        return context.Response.WriteAsync(message);
+    }
+}
--- CartingService/src/CartingService.Api/Program.cs
+++ CartingService/src/CartingService.Api/Program.cs
@@ -1,4 +1,5 @@
 using CartingService.Api.Extensions;
+using CartingService.Api.Middlewares;
 using CartingService.Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -20,7 +21,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+
 app.UseHttpsRedirection();
+
+app.UseRouting();
 
 app.UseAuthorization();
 
